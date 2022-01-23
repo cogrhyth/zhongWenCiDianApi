e53abb9f64bd058780e5a63e2cd74d3946369e57
jonathan.navarro.7e4 <jonathan.navarro.7e4@itb.cat>
2022-01-23 22:47:57 +0100

    requirements.txt updated, endpoints updated

--- main.py
+++ main.py
@@ -1,37 +1,35 @@
-from flask import Flask, request
-import json, time
-
+from flask import Flask, render_template, request
+import json
+import time
 
 application = Flask(__name__)
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
-# database = SQLAlchemy(app)
 
 
-# class Word(database.Model):
-#     id = database.Column(database.Integer, primary_key=True)
-#     english = database.Column(database.String(80), unique=False, nullable=False)
-#     pinyin = database.Column(database.String(80), unique=False, nullable=False)
-#     han_zi = database.Column(database.String(80), unique=False, nullable=False)
+@application.route("/words", methods=["POST"])
+def add_word():
+    if request.method == "POST":
+        english = request.form["english"]
+        pinyin = request.form["pinyin"]
+        han_zi = request.form["hanzi"]
+
+        dictionary = {"english": english, "pinyin": pinyin, "hanzi": han_zi}
+        json_data = json.dumps(dictionary)
+
+        return json_data
 
 
-@application.route("/", methods=["GET"])
-def home_page():
-    data_set = {"Page": "Home", "Message": "Successfully loaded the Home page", "Timestamp": time.time()}
-    json_dump = json.dumps(data_set)
+@application.route("/words", methods=["GET"])
+def get_words():
+    if request.method == "GET":
+        data_set1 = {"Page": "Home", "Message": "Successfully loaded the Home page", "Timestamp": time.time()}
+        data_set2 = {"Page": "Home", "Message": "Successfully loaded the Home page", "Timestamp": time.time()}
+        data_set3 = {"Page": "Home", "Message": "Successfully loaded the Home page", "Timestamp": time.time()}
+        data_sets = {"first": data_set1, "second": data_set2, "third": data_set3}
+        json_data = json.dumps(data_sets)
 
-    return json_dump
-
-
-@application.route("/user/", methods=["GET"])
-def request_page():
-    user_query = str(request.args.get("user"))
-
-    data_set = {"Page": "Request", "Message": f"Successfully got the request for {user_query}", "Timestamp": time.time()}
-    json_dump = json.dumps(data_set)
-
-    return json_dump
+        return json_data
 
 
 if __name__ == "__main__":
-    # database.create_all()
-    application.run(port=7770)
+    application.debug = True
+    application.run(port=7777)
